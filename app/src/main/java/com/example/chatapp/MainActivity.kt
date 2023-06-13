04bf03063af39ecc748283aa8485271bca59d105
JB <108816562+GarethJB@users.noreply.github.com>
2023-06-13 10:04:58 +0900

    update

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -4,7 +4,9 @@ package com.example.chatapp
 import com.example.base.BaseActivity
 import com.example.chatapp.databinding.ActivityMainBinding
 import com.example.chatapp.home.HomeFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     override fun createBinding() = ActivityMainBinding.inflate(layoutInflater)
